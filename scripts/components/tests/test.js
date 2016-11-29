1075819986e0c6aeb29284555f80a1d647af93ae
Bogdan <bogdanparshencev@gmail.com>
2016-11-29 17:42:07 +0200

    Set information for test

--- scripts/components/tests/test.js
+++ scripts/components/tests/test.js
@@ -13,21 +13,22 @@ class Test extends Component {
   render() {
     const { model } = this.props;
     const { tests } = this.props.s;
+    debugger
 
     return (
       <li>
         <Row>
           <Column large={4}>
             <Hash k='Name' v={model.name} />
-            <Hash k='Time Limit' v={model.totalTimeLimit} />
+            <Hash k='Time Limit' v={model.timeLimit} />
           </Column>
           <Column large={3}>
-            <Hash k='Sections' v='' />
-            <Hash k='Max Score' v='' />
+            <Hash k='Sections' v={model.numsOfSections} />
+            <Hash k='Max Score' v={model.maxScore} />
           </Column>
           <Column large={3}>
-            <Hash k='Total Questions' v='' />
-            <Hash k='Shuffle Questions' v='' />
+            <Hash k='Total Questions' v={model.totalQuestions} />
+            <Hash k='Shuffle Questions' v={model.shuffleQuestions} />
           </Column>
           <Column large={2}>
             <div className="button-group small float-right">
