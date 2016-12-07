385ba11cd93e1c3994de790fc3f6c56776c2ed56
Bogdan <bogdanparshencev@gmail.com>
2016-12-07 16:23:04 +0200

    Add J-toker

--- scripts/stores/session.js
+++ scripts/stores/session.js
@@ -4,6 +4,7 @@ import ui     from "./ui";
 import router from "./router";
 import User   from "./../models/user"
 import ajax   from "./../helpers/ajax";
+import auth   from "./../helpers/j-toker-config";
 
 class SessionStore {
 
@@ -16,12 +17,16 @@ class SessionStore {
   }
 
   @action create() {
-    return ajax({ prefix: false, url: '/auth/sign_in', method: 'POST', payload: { user: this.currentUser.serialize() } }).then(
+    auth.emailSignIn({
+      email:    this.currentUser.email,
+      password: this.currentUser.password
+    }).then(
       ({ data }) => {
         this.currentUser.email = data.email
         this.currentUser.firstNameEng = data.firstNameEng
         this.currentUser.lastNameEng  = data.lastNameEng
         this.signInForm(false);
+        router.navigate('dashboard');
       }
     );
   }
